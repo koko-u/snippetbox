252a0327e75314b17234b699b12a36f2f2297973
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-08-26 23:01:45 +0900

    ルーティングを２つ追加する

--- main.go
+++ main.go
@@ -7,9 +7,20 @@ import (
 
 // "Hello from Snippetbox" と返却する home ハンドラを定義する
 func home(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Hello from Snippetbox"))
-	if err != nil {
+	if _, err := w.Write([]byte("Hello from Snippetbox")); err != nil {
 		log.Fatalf("error occurred in home handler: %w", err)
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	if _, err := w.Write([]byte("Display a specific snippet...")); err != nil {
+		log.Fatalf("error occurred in showSnippet handler: %w", err)
+	}
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if _, err := w.Write([]byte("Create a new snippet...")); err != nil {
+		log.Fatalf("error occurred in createSnippet handler: %w", err)
 	}
 }
 
@@ -17,6 +28,8 @@ func main() {
 	// 新しい servemux を作成して、/ に対して home ハンドラを割り当てます
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
 	if err := http.ListenAndServe(":4000", mux); err != nil {
